4240fe40279612602bc64e03a52d7f5db6764d36
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2023-08-31 21:46:51 +0900

    백준 문자열 2

---
+++ src/main/java/org/example/백준/문자열/단어길이재기_2743/Main.java
@@ -0,0 +1,17 @@
+package org.example.백준.문자열.단어길이재기_2743;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String st = br.readLine();
+
+        System.out.println(st.length());
+
+    }
+}
