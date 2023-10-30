994e8c23fd5d987a9d17c6799d236ce6a457f228
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2023-10-30 18:14:38 +0900

    백준 기하: 직사각형과 삼각형 1

---
+++ src/main/java/org/example/백준/기하_직사각형과삼각형/직사각형_27323/Main.java
@@ -0,0 +1,17 @@
+package org.example.백준.기하_직사각형과삼각형.직사각형_27323;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        System.out.println(a*b);
+
+    }
+}
