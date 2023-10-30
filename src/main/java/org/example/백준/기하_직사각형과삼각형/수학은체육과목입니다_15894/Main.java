ccbb45e5fa81ac462b3a4ef602755cbc838df8af
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2023-10-30 19:11:41 +0900

    백준 기하: 직사각형과 삼각형 4
    입력 받는 데이터가 int 범위를 넘어가지 않아도 출력할 때 곱하기를 하면 범위가 넘어감으로 데이터 타입을 long 로 받아야함

---
+++ src/main/java/org/example/백준/기하_직사각형과삼각형/수학은체육과목입니다_15894/Main.java
@@ -0,0 +1,17 @@
+package org.example.백준.기하_직사각형과삼각형.수학은체육과목입니다_15894;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long count = Integer.parseInt(br.readLine());
+
+        System.out.println(count*4);
+
+    }
+}
