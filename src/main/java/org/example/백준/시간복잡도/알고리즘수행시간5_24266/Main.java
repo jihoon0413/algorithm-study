ace82774ae148437185d1e9f630a88e1250bef73
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2023-11-08 20:55:25 +0900

    백준 시간복잡도 5

---
+++ src/main/java/org/example/백준/시간복잡도/알고리즘수행시간5_24266/Main.java
@@ -0,0 +1,19 @@
+package org.example.백준.시간복잡도.알고리즘수행시간5_24266;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long a = Long.parseLong(br.readLine());
+
+
+        System.out.println(a*a*a);
+        System.out.println(3);
+
+    }
+}
