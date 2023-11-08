eb6b483dfa7c8c492f6226b7241fa9d4fe6257d0
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2023-11-08 20:55:01 +0900

    백준 시간복잡도 1

---
+++ src/main/java/org/example/백준/시간복잡도/알고리즘수행시간1_24262/Main.java
@@ -0,0 +1,9 @@
+package org.example.백준.시간복잡도.알고리즘수행시간1_24262;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
