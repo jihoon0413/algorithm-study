f3f09b5bdde604ed3f9ec6b78aae12f93406230c
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2023-11-21 18:19:04 +0900

    백준 자료구조1 3
    1번: 내 풀이
    2번: 인터넷 참고 stack 이용한 풀이

---
+++ src/main/java/org/example/백준/자료구조1/괄호_9012/Main.java
@@ -0,0 +1,53 @@
+package org.example.백준.자료구조1.괄호_9012;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < a; i++) {
+            String vps = br.readLine();
+            int count = vps.length();
+            boolean ans = true;
+            int open = 0;
+            int close = 0;
+
+            if (count % 2 == 0) {
+                for (int j = 0; j < count; j++) {
+                    if (vps.charAt(j) == '(') {
+                        open++;
+                        ans = false;
+                    } else if (vps.charAt(j) == ')') {
+
+                        close++;
+
+                        if (open < close) {
+                            ans = false;
+                            break;
+                        }
+
+                        if (open == close) {
+                            ans = true;
+                        }
+                    }
+
+                }
+                if (open == close && close == count / 2 && ans) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
+
+
