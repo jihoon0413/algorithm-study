03356d83464b5dc4ba35b041832ca5b317c514e9
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2023-11-28 20:15:21 +0900

    백준 자료구조1 7
    1: Listiterator를 사용( 내 아이디어)
    2: Queue를 이용한 풀이(알고리즘을 짜기 더 간단했다)

---
+++ src/main/java/org/example/백준/자료구조1/요세푸스문제_1158/Main.java
@@ -0,0 +1,61 @@
+package org.example.백준.자료구조1.요세푸스문제_1158;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        LinkedList<Integer> linked = new LinkedList<>();
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        for(int i = 0 ; i < a ; i++) {
+            linked.add(i+1);
+        }
+
+        ListIterator<Integer> iterator = linked.listIterator();
+
+        while (!linked.isEmpty()) {
+
+            for(int i = 0 ; i < b ; i++) {
+                if(!iterator.hasNext()){
+                    while (iterator.hasPrevious()){
+                        iterator.previous();
+                    }
+                }
+                    iterator.next();
+            }
+            if(!iterator.hasPrevious()){
+                while(iterator.hasNext()){
+                    iterator.next();
+                }
+            }
+
+            sb.append(iterator.previous());
+            iterator.remove();
+
+            if(linked.isEmpty()){
+                break;
+            } else {
+                sb.append(", ");
+            }
+
+        }
+
+        sb.append(">");
+        System.out.println(sb);
+
+
+    }
+}
---
+++ src/main/java/org/example/백준/자료구조1/요세푸스문제_1158/Main2.java
@@ -0,0 +1,40 @@
+package org.example.백준.자료구조1.요세푸스문제_1158;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Queue<Integer> queue = new LinkedList<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        sb.append("<");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        for(int i = 0 ; i < a ; i++) {
+            queue.offer(i+1);
+        }
+
+        while (queue.size() != 1) {
+
+            for(int i = 0 ; i < b-1 ; i++) {
+                queue.offer(queue.poll());
+            }
+
+            sb.append(queue.poll()).append(", ");
+
+        }
+
+        sb.append(queue.poll()).append(">");
+
+        System.out.println(sb);
+
+    }
+}
