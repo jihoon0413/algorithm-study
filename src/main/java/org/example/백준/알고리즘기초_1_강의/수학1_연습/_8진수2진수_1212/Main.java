e3948ce9ace0435e6646ebaffa22464ad8389036
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-01-24 22:12:31 +0900

    백준 수학1(연습) 4

---
+++ src/main/java/org/example/백준/알고리즘기초_1_강의/수학1_연습/_8진수2진수_1212/Main.java
@@ -0,0 +1,40 @@
+package org.example.백준.알고리즘기초_1_강의.수학1_연습._8진수2진수_1212;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+
+        String input = br.readLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            int a = Integer.parseInt(String.valueOf(input.charAt(i)));
+
+            for (int j = 0; j < 3; j++) {
+                stack.push(a % 2);
+                a /= 2;
+            }
+
+            while (!stack.isEmpty()) {
+                sb.append(stack.pop());
+            }
+        }
+
+        int index = 0;
+        while (sb.charAt(0) == '0') {
+            index++;
+            if (index == 3) {
+                break;
+            }
+            sb.deleteCharAt(0);
+        }
+        System.out.println(sb);
+    }
+}
