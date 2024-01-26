6fc728b1f26f406b8f282828f39d7b914ef380df
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-01-26 21:00:04 +0900

    백준 수학1(연습) 5

---
+++ src/main/java/org/example/백준/알고리즘기초_1_강의/수학1_연습/마이너스2진수_2089/Main.java
@@ -0,0 +1,59 @@
+package org.example.백준.알고리즘기초_1_강의.수학1_연습.마이너스2진수_2089;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Long> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+//        long a = Long.parseLong(br.readLine());
+//
+//        if(a != 0) {
+//            while (a != 1) {
+//                if (a < 0) {
+//                    stack.push(Math.abs(a % -2));
+//                    if(a % -2 == 0) {
+//                        a /= -2;
+//                    } else {
+//                        a = a / -2 + 1;
+//                    }
+//                } else {
+//                    stack.push(a % -2);
+//                    a /= -2;
+//                }
+//            }
+//        }
+//
+//        stack.push(a);
+//
+//        while (!stack.isEmpty()) {
+//            sb.append(stack.pop());
+//        }
+//        System.out.println(sb);
+
+        long a = Long.parseLong(br.readLine());
+
+        if(a != 0) {
+            while (a != 1) {
+                    stack.push(Math.abs(a % -2));
+
+                    a = (long) Math.ceil((double) a / -2);
+
+            }
+        }
+
+        stack.push(a);
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        System.out.println(sb);
+
+    }
+}
