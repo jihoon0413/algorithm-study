360e9366569217a41ce269c54b73497e23c7d976
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-05-07 21:33:30 +0900

    백준 N과M 1

---
+++ src/main/java/org/example/백준/알고리즘기초_2_강의/N과M/_1_15649/Main.java
@@ -0,0 +1,48 @@
+package org.example.백준.알고리즘기초_2_강의.N과M._1_15649;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static boolean[] arr;
+    static StringBuilder sb;
+    static int[] temp;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr = new boolean[N+1];
+        temp = new int[N+1];
+
+        dfs(N,M, 0);
+
+        System.out.println(sb);
+    }
+    static void dfs(int N, int M, int count) {
+
+        if(count == M) {
+            for(int i = 1; i <= M ; i++) {
+                System.out.print(temp[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = 1; i <= N ; i++) {
+            if(!arr[i]) {
+                arr[i] = true;
+                temp[count+1] = i;
+                dfs(N,M,count+1);
+                arr[i] = false;
+            }
+        }
+
+    }
+}
