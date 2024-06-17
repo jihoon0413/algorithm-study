c09be47471e83dace768bbb428dcf15d4158b4d6
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-06-17 22:25:05 +0900

    백준 기타문제
    나만의 풀이로 문제 해결
    10
    1 2 3 4 5 10 7 8 9 10 예제에서 문제 발견 7 이 들어갈 때 기존 식에서는 7을 stack에 넣지 않고 지나갔지만 수정 풀이로는 while문 이 끝나면 stack에 넣어주는 코드를 추가

--- src/main/java/org/example/백준/기타문제/히스토그램_1725/Main.java
+++ src/main/java/org/example/백준/기타문제/히스토그램_1725/Main.java
@@ -8,86 +8,86 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//
-//        Stack<int[]> stack = new Stack<>();
-//
-//        int N = Integer.parseInt(br.readLine());
-//
-//        stack.push(new int[]{0, Integer.parseInt(br.readLine())});
-//        int ans = stack.peek()[1];
-//
-//        for(int i = 1 ; i < N ; i++) {
-//
-//            int temp = Integer.parseInt(br.readLine());
-//
-//            if(stack.peek()[1] < temp) {
-//                stack.push(new int[] {i , temp});
-//                break;
-//            }
-//
-//            while(stack.peek()[1] >= temp) {
-//                int[] top = stack.peek();
-//                if(top[1] == temp) {
-//                    stack.pop();
-//                    stack.push(new int[] {top[0], temp});
-//                    break;
-//                }
-//
-//                stack.pop();
-//                int temp3 = (i-top[0]) * top[1];
-//                ans = Math.max(ans, temp3);
-//
-//                if(stack.isEmpty()) {
-//                    stack.push(new int[] {top[0], temp});
-//                    break;
-//                }
-//            }
-//
-//
-//
-//        }
-//
-//        while(!stack.isEmpty()) {
-//            int[] temp = stack.pop();
-//            ans = Math.max(ans, (N-temp[0])*temp[1]);
-//        }
-//        System.out.println(ans);
-
-
-
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        Stack<int[]> stack = new Stack<>();
+
         int N = Integer.parseInt(br.readLine());
-        int[] height = new int[N+2];
+        int ans = 0;
 
-        long answer = 0;
-        Stack<Integer> stack = new Stack<>();
+        stack.push(new int[]{0, Integer.parseInt(br.readLine())});
+        ans = stack.peek()[1];
 
-        height[0] = 0;
-        height[N] = 0;
+        for(int i = 1 ; i < N ; i++) {
 
-        for(int i = 1; i <= N; i++)
-            height[i] = Integer.parseInt(br.readLine());
+            int temp = Integer.parseInt(br.readLine());
 
-        stack.push(0);
+            if (stack.peek()[1] >= temp ) {
+                int[] temp2 = new int[0];
+                while(stack.peek()[1] >= temp) {
+                    if(stack.peek()[1] == temp) {
+                        temp2 = stack.pop();
+                        stack.push(new int[] {temp2[0], temp});
+                        break;
+                    }
 
-        for(int i = 1; i <= N+1; i++) {
-            while(!stack.isEmpty()) {
-                int top = stack.peek();
+                    temp2 = stack.pop();
+                    int temp3 = (i-temp2[0]) * temp2[1];
+                    ans = Math.max(ans, temp3);
 
-                if(height[top] <= height[i])
-                    break;
-
-                stack.pop();
-                answer = Math.max(answer, (long) height[top] * (i-stack.peek()-1));
+                    if(stack.isEmpty()) {
+                        stack.push(new int[] {temp2[0], temp});
+                        break;
+                    }
+                }
+                stack.push(new int[] {temp2[0], temp});
+            } else {
+                stack.push(new int[] {i , temp});
             }
 
-            stack.push(i);
+
         }
 
-        System.out.println(answer);
+        while(!stack.isEmpty()) {
+            int[] temp = stack.pop();
+            ans = Math.max(ans, (N-temp[0])*temp[1]);
+        }
+        System.out.println(ans);
+
+
+
+
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int N = Integer.parseInt(br.readLine());
+//        int[] height = new int[N+2];
+//
+//        long answer = 0;
+//        Stack<Integer> stack = new Stack<>();
+//
+//        height[0] = 0;
+//        height[N] = 0;
+//
+//        for(int i = 1; i <= N; i++)
+//            height[i] = Integer.parseInt(br.readLine());
+//
+//        stack.push(0);
+//
+//        for(int i = 1; i <= N+1; i++) {
+//            while(!stack.isEmpty()) {
+//                int top = stack.peek();
+//
+//                if(height[top] <= height[i])
+//                    break;
+//
+//                stack.pop();
+//                answer = Math.max(answer, (long) height[top] * (i-stack.peek()-1));
+//            }
+//
+//            stack.push(i);
+//        }
+//
+//        System.out.println(answer);
 
     }
 }
